bc3701c57b6ee941ce0e5565a77a61b6ae89fea3
unknown <an-amo@outlook.sa>
2019-08-26 02:42:54 +0300

    day3

---
+++ day_003.js
@@ -0,0 +1,13 @@
+var a = 5
+document.write(a)
+document.write("<br>")
+
+var b
+b=8
+document.write(b)
+document.write("<br>")
+var c='hi'
+document.write(c)
+document.write("<br>")
+var h="hello world"
+document.write(h)
