8af7ef70089ccab595d417397fe4c9813b462014
unknown <an-amo@outlook.sa>
2019-08-26 02:55:30 +0300

    day4&5

---
+++ day_004 & day_005.js
@@ -0,0 +1,4 @@
+var x = 10
+document.write("Hello, this is my first challenge and I will do it " + x + " times. <br> I will do it, I will learn JavaScript.")
+
+document.write('<p><strong>Week One Done</strong></p>')
