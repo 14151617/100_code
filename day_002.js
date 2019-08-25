cd5145db0446b834c558ee9952492417323cbe73
unknown <an-amo@outlook.sa>
2019-08-26 02:30:10 +0300

    day2

---
+++ day_002.js
@@ -0,0 +1,7 @@
+//this is my first code
+document.write(" مرحبا بكم في عالم الجافا سكربت <br>")
+/*this is my first code 
+and i need to write more comment
+document.write("النص هنا لا تمم طباعته لأنه بداخل التعليق")
+*/
+document.write("مرحبا بكم في عالم الجافا سكربت")
